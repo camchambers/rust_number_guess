d1265093c18ba440fb114301c88cbcf96247fbeb
Cam <cam_chambers@outlook.com>
2022-12-26 08:51:25 -0500

    Take user guess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Pick a number between 1 and 100.");
+    
+    println!("Please enter your guess:");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Error: unable to read your guess.");
+
+    println!("You guessed the number: {guess}");
 }
