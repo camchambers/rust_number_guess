5f9cbd5f0c9c9d03af04dd557d9e936dc2c484fc
Cam <cam_chambers@outlook.com>
2022-12-26 20:57:27 -0500

    Guess comparison

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Pick a number between 1 and 100.");
-    
+
+    let winning_number = rand::thread_rng().gen_range(1..=100);
+        
     println!("Please enter your guess:");
 
     let mut guess = String::new();
@@ -11,5 +15,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Error: unable to read your guess.");
 
+    let mut guess: u32 = guess.trim().parse().expect("Please type a number between 1 and 100.");
+
     println!("You guessed the number: {guess}");
+
+    match guess.cmp(&winning_number){
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("Congratulations! You guessed the winning number"),
+    }
 }
